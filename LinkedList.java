19ff1cae9d4eb1b5bd2a5433ef476fe3010a9f44
Omri Ben atar <omriben@Omris-Air.lan>
2025-01-27 12:00:35 +0200

    testing linkedlist 9

--- LinkedList.java
+++ LinkedList.java
@@ -201,11 +201,14 @@ public class LinkedList {
 	 *         if index is negative or greater than or equal to size
 	 */
 	public void remove(int index) {
-		if (index < 1 || index > size) {
+		if (index < 0 || index > size) {
 			throw new IllegalArgumentException("Index must be between 0 and size.");
 		}
-		if (index ==  1) {
+		if (index ==  0) {
 			first = first.next;
+			if (size == 1) {
+				last = null;
+			}
 		}else {
 			Node prev = getNode(index - 1);
 			prev.next = prev.next.next;
